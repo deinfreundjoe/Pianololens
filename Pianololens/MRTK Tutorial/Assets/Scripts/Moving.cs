830281e98fc03c3a0c78214044bba0f12f1e3505
deinfreundjoe <73248339+deinfreundjoe@users.noreply.github.com>
2020-11-16 12:14:41 +0100

    Adjusted Piano Scene
    Trying to bring in QR Code scanning next

---
+++ Pianololens/MRTK Tutorial/Assets/Scripts/Moving.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moving : MonoBehaviour
+{
+    private float speed = 0.5f;
+    private Vector3 originalPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalPos = gameObject.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GlobalEventHandler.start)
+        {
+            gameObject.transform.position += gameObject.transform.forward * (speed * Time.deltaTime);
+        }
+
+
+        if (GlobalEventHandler.restart)
+        {
+            gameObject.transform.position = originalPos;
+        }
+
+    }
+
+}
